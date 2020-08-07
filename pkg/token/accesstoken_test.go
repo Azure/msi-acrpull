42c53dc205ea4772487d086967d9860908cd0a5e
Sam Kreter <samkreter@gmail.com>
2020-08-07 09:39:47 -0500

    added support for arm and acr tokens

--- pkg/token/accesstoken_test.go
+++ pkg/token/accesstoken_test.go
@@ -29,8 +29,18 @@ var _ = Describe("Access Token Tests", func() {
 	})
 
 	Context("GetTokenTenantID", func() {
-		It("Get Valid Tenant ID", func(){
-			token, err := getTestToken(time.Now().Add(time.Hour).Unix(), signingKey)
+		It("Get Valid Tenant ID Arm Token", func(){
+			token, err := getTestArmToken(time.Now().Add(time.Hour).Unix(), signingKey)
+			Expect(err).ToNot(HaveOccurred())
+
+			tenantID, err := token.GetTokenTenantId()
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(tenantID).To(Equal(testTenantID))
+		})
+
+		It("Get Valid Tenant ID ACR Token", func(){
+			token, err := getTestAcrToken(time.Now().Add(time.Hour).Unix(), signingKey)
 			Expect(err).ToNot(HaveOccurred())
 
 			tenantID, err := token.GetTokenTenantId()
@@ -41,21 +51,55 @@ var _ = Describe("Access Token Tests", func() {
 	})
 
 	Context("GetTokenExp", func() {
-		It("", func(){
+		It("Retrieves Correct Exp Time from ARM Token", func(){
 			expExpected := time.Now().Add(time.Hour).Unix()
 
-			token, err := getTestToken(expExpected, signingKey)
+			token, err := getTestArmToken(expExpected, signingKey)
 			Expect(err).ToNot(HaveOccurred())
 
 			expActual, err := token.GetTokenExp()
 			Expect(err).ToNot(HaveOccurred())
+			Expect(expExpected).To(Equal(expActual.Unix()))
+		})
 
+		It("Retrieves Correct Exp Time from ACR Token", func(){
+			expExpected := time.Now().Add(time.Hour).Unix()
+
+			token, err := getTestAcrToken(expExpected, signingKey)
+			Expect(err).ToNot(HaveOccurred())
+
+			expActual, err := token.GetTokenExp()
+			Expect(err).ToNot(HaveOccurred())
 			Expect(expExpected).To(Equal(expActual.Unix()))
 		})
 	})
 })
 
-func getTestToken(exp int64, signingKey *rsa.PrivateKey) (AccessToken, error){
+func getTestArmToken(exp int64, signingKey *rsa.PrivateKey) (AccessToken, error){
+	claims := jwt.MapClaims{
+		"aud":    "https://management.azure.com/",
+		"exp":    exp,
+		"grant_type": "refresh_token",
+		"iat":    time.Now().AddDate(0, 0, -2).Unix(),
+		"sub": testSubID,
+		"ver": 1.0,
+		"tid": testTenantID,
+		"xms_mirid": "fake/msi/resource/id",
+		"jti": "bb8d6d3d-c7b0-4f96-a390-8738f730e8c6",
+		"iss":    "https://sts.windows.net/"+testTenantID+"/",
+		"nbf":    time.Now().AddDate(0, 0, -1).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256,claims)
+	tokenString, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", err
+	}
+
+	return AccessToken(tokenString), nil
+}
+
+func getTestAcrToken(exp int64, signingKey *rsa.PrivateKey) (AccessToken, error){
 	claims := jwt.MapClaims{
 		"aud":    "test.azurecr.io",
 		"exp":    exp,
@@ -64,6 +108,7 @@ func getTestToken(exp int64, signingKey *rsa.PrivateKey) (AccessToken, error){
 		"sub": testSubID,
 		"version": 1.0,
 		"tenant": testTenantID,
+		"tid": testTenantID,
 		"permissions": map[string]interface{} {
 			"actions": []string{"read"},
 		},
