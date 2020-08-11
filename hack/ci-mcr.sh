9d23ee87c9d696fa332a2cffdb3c05f913b8e9e3
Wei Weng <weweng@microsoft.com>
2020-08-11 10:07:00 -0700

    address comment

--- hack/ci-mcr.sh
+++ hack/ci-mcr.sh
@@ -13,7 +13,12 @@ docker login ${MCR_REGISTRY} -u ${MCR_USERNAME} -p ${MCR_PASSWORD}
 TAG=${BUILD_NUMBER}
 echo "image tag will be ${TAG}"
 
-docker build . -t "${REGISTRY}/${APP}:${TAG}"
+IMG="${REGISTRY}/${APP}:${TAG}"
+make docker-build
 docker tag "${REGISTRY}/${APP}:${TAG}" "${MCR_REGISTRY}${MCR_ORG}/${APP}:${TAG}"
 docker push "${REGISTRY}/${APP}:${TAG}"
 docker push "${MCR_REGISTRY}${MCR_ORG}/${APP}:${TAG}"
+
+# push to latest tag as well
+docker tag "${REGISTRY}/${APP}:${TAG}" "${MCR_REGISTRY}${MCR_ORG}/${APP}:latest"
+docker push "${MCR_REGISTRY}${MCR_ORG}/${APP}:latest"
