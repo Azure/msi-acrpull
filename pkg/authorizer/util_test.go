64462b701f2b6c59f445c226d367d06fe7900c82
Anders Liu <yangl@microsoft.com>
2020-08-12 01:40:49 -0700

    test: add util tests

---
+++ pkg/authorizer/util_test.go
@@ -0,0 +1,26 @@
+package authorizer
+
+import (
+	"encoding/json"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Util Tests", func() {
+
+	Context("Generate Docker Config", func() {
+		It("Generate correct Docker Config JSON", func() {
+			acrToken, err := getTestAcrToken(time.Now().Add(time.Hour).Unix(), signingKey)
+			Expect(err).ToNot(HaveOccurred())
+
+			cfg := CreateACRDockerCfg(testACR, acrToken)
+
+			var cfgJSON interface{}
+			err = json.Unmarshal([]byte(cfg), &cfgJSON)
+
+			Expect(err).To(BeNil())
+		})
+	})
+})
